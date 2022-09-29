c26a483992dd0d3984f995f63f09a451f6adbbd5
fawsiyyah <fawsiyyahlamidi@gmail.com>
2022-09-29 09:03:39 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,91 @@
+function myFunction() {
+    var dots = 
+    document.getElementById("dots");
+    var moreText = 
+    document.getElementById("more");
+    var btnText = 
+    document.getElementById("myBtn");
+
+    if (dots.style.display === "none"){
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read Comments";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Close comments";
+        moreText.style.display = "inline";
+    }
+}
+
+function myFunction1() {
+    var dots = 
+    document.getElementById("dots1");
+    var moreText = 
+    document.getElementById("more1");
+    var btnText = 
+    document.getElementById("myBtn2");
+
+    if (dots.style.display === "none"){
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read Comments";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Close comments";
+        moreText.style.display = "inline";
+    }
+}
+
+function myFunction2() {
+    var dots = 
+    document.getElementById("dots2");
+    var moreText = 
+    document.getElementById("more2");
+    var btnText = 
+    document.getElementById("myBtn3");
+
+    if (dots.style.display === "none"){
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read Comments";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Close comments";
+        moreText.style.display = "inline";
+    }
+}
+
+function myFunction3() {
+    var dots = 
+    document.getElementById("dots3");
+    var moreText = 
+    document.getElementById("more3");
+    var btnText = 
+    document.getElementById("myBtn4");
+
+    if (dots.style.display === "none"){
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read Comments";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Close comments";
+        moreText.style.display = "inline";
+    }
+}
+
+mybutton = document.getElementById("topBtn");
+
+window.onscroll = function(){scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop >20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
